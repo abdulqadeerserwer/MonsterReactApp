1fd14641fbb8c61d32163bae5592179fdebdea06
Abdul Qadeer <a.qadeerserwer55@gmail.com>
2019-08-09 15:08:28 +0500

    'initial'

---
+++ precache-manifest.925d81a3b6ab67fd2ee83cee532a2ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d02ef08d49ebb0666ab1799b815e7f",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "f478fbaa07d507c5aaeb",
+    "url": "/app/static/css/main.21755b7e.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/app/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "f478fbaa07d507c5aaeb",
+    "url": "/app/static/js/main.91352033.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  }
+]);
